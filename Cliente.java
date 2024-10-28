e82d568417247f0b9ba915fed171e36727c76248
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-10-28 20:10:02 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,13 +1,13 @@
 public class Cliente{
-    private String nome;
-    private String cpf;
-    private String email;
-    private String celular;
-    
-    public Cliente(String nome, String cpf, String email, String celular){
-        this.nome=nome;
-        this.cpf=cpf;
-        this.email=email;
-        this.celular=celular;
+        private String nome;
+        private String cpf;
+        private String email;
+        private String celular;
+
+        public Cliente(String nome, String cpf, String email, String celular){
+            this.nome=nome;
+            this.cpf=cpf;
+            this.email=email;
+            this.celular=celular;
+        }
     }
-}
