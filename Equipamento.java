ed480b30edf8bd85856bdff9ff44d6662f25c07d
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-10-29 00:49:14 -0300

    Update Equipamento.java

--- Equipamento.java
+++ Equipamento.java
@@ -1,13 +1,44 @@
-public class Equipamento{
+public class Equipamento {
         private String codigo;
         private String descricao;
         private String status;
         private String situacao;
+        private String responsavel;
 
-        public Equipamento(String codigo, String descricao, String status, String situacao){
-            this.codigo=codigo;
-            this.descricao=descricao;
-            this.status=status;
-            this.situacao=situacao;
+        public Equipamento(String codigo, String descricao, String status, String situacao, String responsavel) {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.status = status;
+            this.situacao = situacao;
+            this.responsavel = responsavel;
+        }
+
+        public String getCodigo() {
+            return codigo;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public String getSituacao() {
+            return situacao;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+        
+        public void setResponsavel(String responsavel) {
+            this.responsavel = responsavel;
+        }
+
+        public void imprimirDetalhes() {
+            System.out.println("Código: " + codigo);
+            System.out.println("Descrição: " + descricao);
+            System.out.println("Status: " + status);
+            System.out.println("Situação: " + situacao);
+            System.out.println("Responsavel: " + responsavel);
+            System.out.println("----------------------");
         }
     }
