9612d8ff31efcca6c7e5111ba696bcaeece4e289
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-10-29 00:48:39 -0300

    Delete Emprestimo.java

--- Emprestimo.java
+++
@@ -1,7 +0,0 @@
-public class Emprestimo{
-        private String responsavel;
-
-        public Emprestimo(String hora, String responsavel){
-            this.responsavel=responsavel;
-        }
-    }
