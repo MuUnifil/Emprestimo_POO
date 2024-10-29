3e706561ab57da0b3c3c74936776d42c1fc3b298
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-10-29 00:51:04 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,28 +1,47 @@
-public class Menu
-    {
-        public void Menu() {
+static class Menu {
+        private Main main;
+
+        public Menu(Main main) {
+            this.main = main; // Armazena a instância de Main passada
             int menu = 1;
             Scanner scanner = new Scanner(System.in);
 
-            while (menu != 0){
-                System.out.println("Seja bem vindo a central de emprestimos, o que deseja fazer ?! \n 1. Cadastrar Funcionário\n 2. Cadastrar Equipamento\n 3. Emprestar Equipamento\n 4. Devolver Equipamento\n 5. Listar Equipamentos\n 0. Sair");
+            while (menu != 0) {
+                System.out.println("Seja bem-vindo à central de empréstimos, o que deseja fazer?\n" +
+                                   "1. Cadastrar Cliente\n" +
+                                   "2. Cadastrar Equipamento\n" +
+                                   "3. Emprestar Equipamento\n" +
+                                   "4. Devolver Equipamento\n" +
+                                   "5. Listar Equipamentos\n" +
+                                   "0. Sair");
 
                 menu = scanner.nextInt();
+                scanner.nextLine(); // Limpar o buffer
 
-                if (menu == 1){
-                    adicionarPessoa();}
-                else if (menu == 2){
-                    cadastrarEquipamento();}
-                else if (menu == 3){
-                    logicaEmprestimo();}
-                else if (menu == 4){
-                    devolver_equipamento();}
-                else if (menu == 5){
-                    Listar();}
-                else if (menu == 6){
-                    System.out.println("Saindo...");}
-                else{
-                    System.out.print("\nO que você digitou está errado, digite um dos numeros pedidos");}
+                switch (menu) {
+                    case 1:
+                        main.adicionarPessoa();
+                        break;
+                    case 2:
+                        main.cadastrarEquipamento();
+                        break;
+                    case 3:
+                        main.logicaEmprestimo();
+                        break;
+                    case 4:
+                        main.devolverEquipamento();
+                        break;
+                    case 5:
+                        main.listar();
+                        break;
+                    case 0:
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
+                        break;
+                }
             }
+            scanner.close();
         }
     }
